285db49e954f9ea9398268d7e45ff211549687af
Douglas Gioielli <douglasgioielli@gmail.com>
2022-04-19 14:58:34 -0300

    *** Alteração na moontagem do MapWidget

--- RPGTools/modules/MapWidget/mapwidget.py
+++ RPGTools/modules/MapWidget/mapwidget.py
@@ -1,3 +1,4 @@
+from ctypes import sizeof
 from turtle import width
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -8,11 +9,12 @@ from kivy.core.window import Window
 class MapWidget(ScrollView):
     
     def __init__(self, **kwargs):
-        super().__init__(size_hint=(1, None), size=(Window.width, Window.height), **kwargs)
-        layout = GridLayout(cols=10, spacing=10, size_hint=(None, None))
+        super().__init__(size_hint=(1, 1), **kwargs)
+        layout = GridLayout(cols=12, spacing=10, size_hint=(None, None), padding=[20, 20, 20, 20])
         layout.bind(minimum_height=layout.setter("height"))
-        for i in range(100):
-            btn = Button(text=str(i), size_hint=(None, None), height=40, width=40)
+        layout.bind(minimum_width=layout.setter("width"))
+        for i in range(144):
+            btn = Button(text=str(i), size_hint=(None, None), height=80, width=80)
             layout.add_widget(btn)
         self.add_widget(layout)
         
